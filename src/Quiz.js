35c2f6fa390601aeae239c13a2aee2df71ce3136
User <golib94jon@gmail.com>
2020-11-04 12:34:22 +0500

    init

---
+++ src/Quiz.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import {QuizData} from './quizData';
+import './style.css';
+
+export class Quiz extends Component {
+
+constructor(props) {
+    super(props)
+
+    this.state = {
+         userAnswer: null,
+         currentIndex: 0,
+         options: [],
+         quizEnd: false,
+         score: 0,
+         disabled: true
+    }
+}
+
+    loadQuiz = () => {
+        const{currentIndex} = this.state;
+        this.setState(() => {
+            return {
+                question: QuizData[currentIndex].question,
+                options: QuizData[currentIndex].options,
+                answer: QuizData[currentIndex].answer
+            }
+        })
+    }
+
+     nextQuestionHandler = () => {
+         const {userAnswer, answer, score} = this.state
+         
+        if(userAnswer === answer){
+            this.setState ({
+                score: score +1
+            })
+        }
+
+         this.setState({
+             currentIndex: this.state.currentIndex + 1,
+             userAnswer: null
+         })
+     }             
+     componentDidMount() {
+         this.loadQuiz();
+     }
+
+     checkAnswer = answer => {
+         this.setState({
+             userAnswer: answer,
+             disabled: false
+         })
+     }
+
+     componentDidUpdate(prevProps, prevState){
+         const{currentIndex} = this.state;
+         if(this.state.currentIndex !== prevState.currentIndex){
+            this.setState(() => {
+                return {
+                    question: QuizData[currentIndex].question,
+                    options: QuizData[currentIndex].options,
+                    answer: QuizData[currentIndex].answer
+                }
+            })
+         }
+     }
+ 
+     finishHandler =() => {
+        if(this.state.currentIndex === QuizData.length -1){
+            this.setState({
+                quizEnd:true
+            })
+        }
+    
+    }
+
+
+   
+    render() {
+      
+        const {question, options, currentIndex, userAnswer, quizEnd} = this.state
+
+        if(quizEnd) {
+            return (
+                <div>
+                    <h1> Tugadi. Yakuniy to'g'ri javob {this.state.score} ta</h1>
+                    <p>Viktorinamiz uchun joriy javoblar!</p>
+                    <ul>
+                        {QuizData.map((item, index) => (
+                            <li className={options}
+                            key={index}>
+                                {item.answer}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
+
+
+
+
+        return (
+            <div>
+                <h2>{question}</h2>
+                <span>{`Savollar: ${currentIndex +1} dan ${QuizData.length} gacha`}</span>
+                {
+                    options.map(option => 
+                        <p key = {option.id} className={`options ${userAnswer === option? "selected" : null}`}
+                        onClick = {() => this.checkAnswer(option)} 
+                        >
+                            {option}
+                        </p>
+                        )
+                    
+                }
+
+                {currentIndex < QuizData.length -1 && 
+                <button disabled = {this.state.disabled} onClick={this.nextQuestionHandler}>
+                   Keyingisi
+                </button>}
+                {currentIndex === QuizData.length - 1 && 
+                <button onClick = {this.finishHandler} disabled = {this.state.disabled}>
+                    Tugadi
+                </button>}
+
+            </div>
+            
+        )
+        
+    }
+}
+
+export default Quiz
---
+++ src/quizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `1 gigabayt necha megabayt bo'ladi?`,
+        options: [`2048`, `1024`, `900`, `1000`],
+        answer: `1024`
+    },
+    {
+        id: 1,
+        question: `O'zbekiston Respublikasi Konstitutsiyasi qachon qabul qilingan?`,
+        options: [`1993-yil`, `1990-yil`, `1992-yil`, `1991-yil`],
+        answer: `1992-yil`
+    },
+    {
+        id: 2,
+        question: `Avstraliya poytaxti nima?`,
+        options: [`Toshkent`, `Moskva`, `Kuala-Lumpur`, `Sidney`],
+        answer: `Sidney`
+    },
+    {
+        id: 3,
+        question: `O'zbekiston Respublikasi qachon Mustaqil bo'lgan?`,
+        options: [`1991-yil`, `1990-yil`, `1989-yil`, `1994-yil`],
+        answer: `1991-yil`
+    },
+    {
+        id: 4,
+        question: ` Amir Temur qachon taxtga o'tirgan?`,
+        options: [`1375-yil`, `1365-yil`, `1370-yil`, `1372-yil`],
+        answer: `1370-yil`
+    },
+] 
